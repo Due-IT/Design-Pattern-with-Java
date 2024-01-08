ab51e53605e4ca87c3ca557416fd8439d21ce6a8
FhRh <yiyop@naver.com>
2024-01-08 12:12:28 +0900

    Frest

---
+++ Structural Patterns/Flyweight/src/forest/Forest.java
@@ -0,0 +1,21 @@
+package forest;
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class Forest extends JFrame {
+    private List<Tree> trees = new ArrayList<>();
+
+    public void plantTree(int x, int y, String name, Color color, String otherTreeData) {
+        TreeType type = TreeFactory.getTreeType(name, color, otherTreeData);
+        Tree tree = new Tree(x, y, type);
+        trees.add(tree);
+    }
+
+    public void plantTree(int x, int y, String name, Color color, String otherTreeData) {
+        TreeType type = TreeFactory.getTreeType(name, color, otherTreeData);
+        Tree tree = new Tree(x, y, type);
+        trees.add(tree);
+    }
+
+}
