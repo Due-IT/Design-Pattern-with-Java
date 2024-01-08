b51c7de48617028fc8658e9016220dffd9fd79b0
FhRh <yiyop@naver.com>
2024-01-08 12:51:55 +0900

    Flyweight

--- Structural Patterns/Flyweight/src/trees/TreeFactory.java
+++ Structural Patterns/Flyweight/src/trees/TreeFactory.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TreeFactory {
-    static Map<String, TreeType> treeTypes = new HashMap<>();
+    static Map<String, TreeType> treeTypes = new HashMap<>(); //공통속성을 저장하는 변수
 
     public static TreeType getTreeType(String name, Color color, String otherTreeData) {
         TreeType result = treeTypes.get(name);
