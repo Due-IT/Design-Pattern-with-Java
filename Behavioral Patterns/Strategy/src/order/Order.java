a1912942e2d648836265b2e845fd4f54d5cdcea2
FhRh <yiyop@naver.com>
2024-01-17 19:08:34 +0900

    PayStrategy

--- Behavioral Patterns/Strategy/src/order/Order.java
+++ Behavioral Patterns/Strategy/src/order/Order.java
@@ -6,7 +6,7 @@ public class Order {
     private int totalCost = 0;
     private boolean isClosed = false;
 
-    public void processOrder(PayStrategy strategy) {
+    public void processOrder(PayStrategy strategy) { //전략을 매개변수로 전달하여 작업 수행
         strategy.collectPaymentDetails();
         // Here we could collect and store payment data from the strategy.
     }
