f4292d43035482174c4433b82688516866fb06fa
FhRh <yiyop@naver.com>
2024-10-28 20:36:02 +0900

    fix(TemplateMethod) : 예외처리

--- Behavioral Patterns/Template Method/src/Main.java
+++ Behavioral Patterns/Template Method/src/Main.java
@@ -6,7 +6,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         Network network = null;
         System.out.print("Input user name: ");
